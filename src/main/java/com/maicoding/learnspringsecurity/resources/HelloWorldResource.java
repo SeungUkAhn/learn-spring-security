e5351b665fa8c96d9a88cbf32a343ad9726a8b6f
SeungUk Ahn <ahn2802@gmail.com>
2023-08-24 04:35:30 +0900

    Todo Resource 생성: CSRF 테스트를 위한 GET, POST 맵핑 생성

--- src/main/java/com/maicoding/learnspringsecurity/resources/HelloWorldResource.java
+++ src/main/java/com/maicoding/learnspringsecurity/resources/HelloWorldResource.java
@@ -1,4 +1,4 @@
-package com.maicoding.learnspringsecurity;
+package com.maicoding.learnspringsecurity.resources;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/main/java/com/maicoding/learnspringsecurity/resources/TodoResource.java
@@ -0,0 +1,34 @@
+package com.maicoding.learnspringsecurity.resources;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TodoResource {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    public static final List<Todo> TODOS_LIST =
+            List.of(new Todo("maicoding", "Learn AWS"),
+                    new Todo("maicoding", "Learn Azure"));
+
+    @GetMapping("/todos")
+    public List<Todo> retrieveAllTodos(){
+        return TODOS_LIST;
+    }
+
+    @GetMapping("/users/{username}/todos")
+    public Todo retrieveTodosForOneUser(@PathVariable String username){
+        return TODOS_LIST.get(0);
+    }
+
+    @PostMapping("/users/{username}/todos")
+    public void createTodoForOneUser(@PathVariable String username, @RequestBody Todo todo){
+        logger.info("Create {} for {}", todo, username);
+    }
+}
+
+record Todo (String username, String description){}
